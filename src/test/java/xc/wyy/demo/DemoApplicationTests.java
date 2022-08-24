c9e645129686f8ba6cb8dc3f1250162b84ec9720
Yingying Wang <wyyxingcheng@163.com>
2022-08-24 19:18:37 +0800

    test

---
+++ src/test/java/xc/wyy/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xc.wyy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
